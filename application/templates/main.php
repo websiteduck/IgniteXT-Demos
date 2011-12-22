cfe1b15ea408bdd424500198effc71d981de9a58
WebsiteDuck <brian@websiteduck.com>
2011-12-22 13:23:01 -0600

    Improve header, add menu

--- application/templates/main.php
+++ application/templates/main.php
@@ -8,9 +8,19 @@
 		<div id="header">
 			<img src="<?=BASEURL?>assets/images/ignitext.png" />
 		</div>
+		<div id="header_menu">
+			<ul>
+				<li><a href="<?=BASEURL?>">Home</a></li>
+				<li><a href="http://www.ignitext.com">IgniteXT</a></li>
+				<li><a href="https://github.com/websiteduck/">GitHub</a></li>
+			</ul>
+		</div>
+		<div id="content">
 		<? \System\Display::view($content_view, $data); ?>
+		</div>
 		<div id="footer">
-			IgniteXT Demos use icons created by <a href="http://p.yusukekamiyamane.com">Yusuke Kamiyamane</a>. All rights reserved. Licensed under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>.
+			<p style="text-align: center;">&copy; 2011 Website Duck LLC</p>
+			<p style="text-align: center;">IgniteXT Demos use icons created by <a href="http://p.yusukekamiyamane.com">Yusuke Kamiyamane</a>. All rights reserved. Licensed under a <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a>.</p>
 		</div>
 	</body>
 </html>
