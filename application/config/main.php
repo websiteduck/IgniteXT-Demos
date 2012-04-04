0931093990d151b61c8695298bb57e87a79d8179
Brian Adams <brian@websiteduck.com>
2012-04-03 23:19:05 -0500

    Changes for external config file

--- application/config/main.php
+++ application/config/main.php
@@ -33,4 +33,18 @@ if (true)
  * Database Configuration
  * \System\Database::connect(identifier, driver, server, username, password, database);
  */
-\System\Database::connect_dsn('main', 'sqlite:ixt_demo.sqlite');
+if (isset($application_config['database']['databases']))
+{
+	foreach ($application_config['database']['databases'] as $db_string)
+	{
+		$db_array = explode(',', $db_string);
+		switch (count($db_array))
+		{
+			case 6:	\System\Database::connect($db_array[0], $db_array[1], $db_array[2], $db_array[3], $db_array[4], $db_array[5]); break;
+			case 4:	\System\Database::connect_dsn($db_array[0], $db_array[1], $db_array[2], $db_array[3]); break;
+			case 3: \System\Database::connect_dsn($db_array[0], $db_array[1], $db_array[2]); break;
+			case 2:	\System\Database::connect_dsn($db_array[0], $db_array[1]); break;
+			default: throw new Exception('Invalid number of parameters in config database settings.'); break;
+		}
+	}
+}
